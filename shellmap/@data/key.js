cc9dd0d7ca83c5d7c4f9a29e1243462ffb55362b
CSharps-Tech <114924195+CSharps-Tech@users.noreply.github.com>
2022-12-20 13:31:44 +0700

    @save need

---
+++ shellmap/@data/key.js
@@ -0,0 +1,26 @@
+const keybind = require("keybind")
+const key = require("./.key")
+const bind = new keybind.Start().config([
+    "PERMISSION_GRANT_CREATE_BIND",
+    "PERMISSION_GRANT_CREATE_BUN"
+])
+
+bind.bun("CTRL-F", {
+   key == true 
+   if(user.passed==true){ 
+    process.exit()
+    save.in()
+}
+});
+bind.bun("CTRL-V", {
+   key == true 
+   if(user.passed==true){ 
+    bind.create("AFK is allowed because you code in cloud storage save in 24/7")
+}
+});
+bind.bun("CTRL-ALT-F1", {
+   key == true 
+   if(user.passed==true){ 
+    UI.join("com.cloud_storage.obyma.hidden")
+}
+});
